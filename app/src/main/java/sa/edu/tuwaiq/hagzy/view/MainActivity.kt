3824143278e745dfd77c5774023dc061c797ba2e
Ghadeer <gadeer.a094@gmail.com>
2021-11-24 10:57:57 +0300

    Started Project

---
+++ app/src/main/java/sa/edu/tuwaiq/hagzy/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package sa.edu.tuwaiq.hagzy.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sa.edu.tuwaiq.hagzy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
