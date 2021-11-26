e78615da462884bd5d2ec56dea2593f28ed14776
Hindalzarah <Hindalzarah@outlook.com>
2021-11-26 15:38:46 +0300

    dao

--- app/src/main/java/sa/edu/tuwaiq/hagzy/database/IFlickerDao.kt
+++ app/src/main/java/sa/edu/tuwaiq/hagzy/database/IFlickerDao.kt
@@ -1,4 +1,19 @@
 package sa.edu.tuwaiq.hagzy.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import sa.edu.tuwaiq.hagzy.model.Photo
+
+
+@Dao
 interface IFlickerDao {
+
+
+    //insert parameters in the database
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPhotos(photos: List<Photo>)
+
+
+
 }
