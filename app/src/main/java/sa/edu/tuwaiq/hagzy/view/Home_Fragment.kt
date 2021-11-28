b6135ea5c960855a812934e50ce2b1ebf14bc728
hanaa <hana.alrashidi.dmm.k1@tuwaiq.edu.sa>
2021-11-28 10:06:07 +0300

    share Edit

--- app/src/main/java/sa/edu/tuwaiq/hagzy/view/Home_Fragment.kt
+++ app/src/main/java/sa/edu/tuwaiq/hagzy/view/Home_Fragment.kt
@@ -6,17 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import sa.edu.tuwaiq.hagzy.R
+import sa.edu.tuwaiq.hagzy.databinding.ActivityMainBinding
+import sa.edu.tuwaiq.hagzy.databinding.FragmentHomeBinding
+import sa.edu.tuwaiq.hagzy.model.PhotoModel
 
 
 class Home_Fragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
+    private var allPhotos = listOf<PhotoModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
