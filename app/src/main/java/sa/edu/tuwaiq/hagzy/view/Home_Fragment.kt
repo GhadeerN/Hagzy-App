1c79a0b46870847f8c3732c11480c826053f36f8
hanaa <hana.alrashidi.dmm.k1@tuwaiq.edu.sa>
2021-11-28 16:09:29 +0300

    share is done by me yaay

--- app/src/main/java/sa/edu/tuwaiq/hagzy/view/Home_Fragment.kt
+++ app/src/main/java/sa/edu/tuwaiq/hagzy/view/Home_Fragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import sa.edu.tuwaiq.hagzy.R
 import sa.edu.tuwaiq.hagzy.databinding.ActivityMainBinding
 import sa.edu.tuwaiq.hagzy.databinding.FragmentHomeBinding
 import sa.edu.tuwaiq.hagzy.model.PhotoModel
+import sa.edu.tuwaiq.hagzy.view.adapters.PhotosRecyclerViewAdapter
+import sa.edu.tuwaiq.hagzy.view.main.PhotosViewModel
 
 
 class Home_Fragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
-    private var allPhotos = listOf<PhotoModel>()
+    private lateinit var photoAdapter: PhotosRecyclerViewAdapter
+    private val photoViewModel: PhotosViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +29,28 @@ class Home_Fragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // we pass productViewModel to use it in favoriteToggleButton in adapter
+        photoAdapter = PhotosRecyclerViewAdapter(requireActivity())
+        binding.homeRecyclerView.adapter = photoAdapter
+
+        observers()
+
+        //Event
+        photoViewModel.callPhotos() // because we want the call when app start so we add it in onViewCreated
+    }
+
+    private fun observers() {
+        photoViewModel.photosLiveData.observe(viewLifecycleOwner, {
+
+            binding.homeProgressBar.animate().alpha(0f).setDuration(1000)
+            photoAdapter.submitList(it.photos.photo)
+            binding.homeRecyclerView.animate().alpha(1f)
+        })
     }
 
 }
