7bf4d65545a6c0844ad9ad33f0c372c058b7740f
= <=>
2021-11-28 15:34:27 +0300

    @arwa alomari MainActivity.kt, ActivityMain_.xml, HomeFragment.kt, fragment_home_.xml, nav_garph.xml, themes.xml,

--- app/src/main/java/sa/edu/tuwaiq/hagzy/view/Home_Fragment.kt
+++ app/src/main/java/sa/edu/tuwaiq/hagzy/view/Home_Fragment.kt
@@ -1,10 +1,9 @@
 package sa.edu.tuwaiq.hagzy.view
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import sa.edu.tuwaiq.hagzy.R
 
 
@@ -17,6 +16,20 @@ class Home_Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_, container, false)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.favorite_item -> {
+                findNavController().navigate(R.id.action_home_Fragment_to_favoriteFragment)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        requireActivity().menuInflater.inflate(R.menu.main_activity_top_app_bar, menu)
     }
 
 }
